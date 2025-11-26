eef21709ccdab4a373666d71c010ef895748eacd
MarioR17 <jmrendonq17@gmail.com>
2025-11-26 18:19:40 -0500

    Adding a test to grab a song from a playlist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import spotipy
+import json
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 
@@ -19,3 +20,28 @@ if user:
     print(f"Authenticated as: {user['display_name']} (ID: {user['id']})")
 else:
     print("Could not retrieve user details")
+
+print("\n--- Fetching Playlists ---")
+playlists = sp.current_user_playlists(limit=5)
+
+if playlists:
+    if not playlists['items']:
+        print("No playlists found.")
+    else:
+        first_playlist = playlists['items'][0]
+        print(f"Testing with Playlist: '{first_playlist['name']}' (ID: {first_playlist['id']})")
+
+        results = sp.playlist_items(first_playlist['id'], limit=1)
+        
+        if results:
+            if len(results['items']) > 0:
+                item = results['items'][0]
+                track = item['track']
+
+                print("\n--- Validated Song Data ---")
+                print(f"Name:   {track['name']}")
+                print(f"Artist: {track['artists'][0]['name']}")
+                print(f"URI:    {track['uri']}")
+                
+            else:
+                print("The first playlist was empty!")
