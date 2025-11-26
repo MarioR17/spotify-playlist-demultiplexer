9c8f8af450a0e38e78d5ca70a2b7b62605164685
MarioR17 <jmrendonq17@gmail.com>
2025-11-26 18:01:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spotify-playlist-demultiplexer
+Little hobby project to take my big spotify playlists and generate sub-playlists based on the genres of the songs
---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+                     scope="playlist-modify-public playlist-modify-private user-library-read"
+))
+
+user = sp.current_user()
+
+if user:
+    print(f"Authenticated as: {user['display_name']} (ID: {user['id']})")
+else:
+    print("Could not retrieve user details")
